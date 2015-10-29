4d9a323c3f44b2830b4acda1a99bc26cf958352c
Wicaeed <wicaeed@gmail.com>
2015-10-29 11:48:09 -0700

    updating git config

--- r10k_installation.pp
+++ r10k_installation.pp
@@ -11,7 +11,7 @@ class { 'r10k':
   version           => '1.5.1',
   sources           => {
     'puppet' => {
-      'remote'  => 'git@github.com:puppetinabox/controlrepo.git',
+      'remote'  => 'git@github.com:wicaeed/controlrepo.git',
       'basedir' => "${::settings::confdir}/environments",
       'prefix'  => false,
     },
