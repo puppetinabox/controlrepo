3663c7fc6d9f9860ddbc8ae560766c51ac8ef564
Craig Gill <wicaeed@gmail.com>
2015-10-29 16:04:29 -0700

    Adding manual versioning to master.pp

--- master.pp
+++ master.pp
@@ -4,7 +4,14 @@ Package {
 
 include ::epel
 Yumrepo<| |> -> Package <| |>
-include ::puppetdb
+
+
+# https://github.com/puppetlabs/puppetlabs-puppetdb#upgrading-from-4x-to-version-5x
+class { '::puppetdb::globals':
+  version => '2.3.7-1.el6',
+}
+include '::puppetdb'
+
 class { '::puppet::master':
   storeconfigs => true,
   environments => directory,
