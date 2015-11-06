30ab7ff400ffeaea415f6664628a636ad09c87bc
Craig Gill <wicaeed@gmail.com>
2015-11-05 21:43:43 -0800

    more fixes

--- master.pp
+++ master.pp
@@ -4,12 +4,6 @@ Package {
 
 include ::epel
 Yumrepo<| |> -> Package <| |>
-
-
-# https://github.com/puppetlabs/puppetlabs-puppetdb#upgrading-from-4x-to-version-5x
-class { '::puppetdb::globals':
-  version => '2.3.7-1.el6',
-}
 include '::puppetdb'
 
 class { '::puppet::master':
